6712cd8fa302bda49d32c40b1565e8698ea5945c
AEU <aeuplayground@gmail.com>
2025-07-20 00:56:06 +0900

    feat: useSearchParams 훅을 위한 suspanse 추가

--- app/posts/edit/page.tsx
+++ app/posts/edit/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from '@/components/Header';
 import PostForm from './PostForm';
+import { Suspense } from 'react';
 
 export default function EditPostPage() {
   return (
@@ -13,7 +14,9 @@ export default function EditPostPage() {
           <h1 className="text-3xl font-bold mb-4">게시글 수정</h1>
           <p className="text-gray-600">여행 경험을 공유하고 다른 여행자들에게 도움을 주세요</p>
         </div>
-        <PostForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PostForm />
+        </Suspense>
       </div>
     </div>
   );
