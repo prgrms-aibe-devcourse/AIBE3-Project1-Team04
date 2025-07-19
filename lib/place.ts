24bf8ae265f524db84ab2519c74d93f13ad340df
AEU <aeuplayground@gmail.com>
2025-07-19 17:24:24 +0900

    chore: 타입 및 일반 유틸함수 정리

--- lib/place.ts
+++ lib/place.ts
@@ -1,8 +1,23 @@
-import { differenceInCalendarDays, parseISO } from 'date-fns';
+import { differenceInCalendarDays, differenceInHours, parseISO } from 'date-fns';
 
 export const getStayDuration = (startDateStr: string, endDateStr: string) => {
   const start = parseISO(startDateStr);
   const end = parseISO(endDateStr);
+
+  const nights = differenceInCalendarDays(end, start);
+  const days = nights + 1;
+
+  return `${nights}박 ${days}일`;
+};
+
+export const getStayDuration_withTime = (startDateStr: string, endDateStr: string) => {
+  const start = parseISO(startDateStr);
+  const end = parseISO(endDateStr);
+
+  const hours = differenceInHours(end, start);
+  if (hours < 24) {
+    return `${hours}시간`;
+  }
 
   const nights = differenceInCalendarDays(end, start);
   const days = nights + 1;
